5f7e72eac2e8c9190e038922db729eb8642504c2
Adrián Tena Burgos <adrian.tena@bosonit.com>
2022-05-23 09:51:36 +0200

    Saludo desde la clase inicial

--- src/main/java/com/adri/bs3/EjercicioBs3SpringApplication.java
+++ src/main/java/com/adri/bs3/EjercicioBs3SpringApplication.java
@@ -3,11 +3,17 @@ package com.adri.bs3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class EjercicioBs3SpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EjercicioBs3SpringApplication.class, args);
+	}
+	@PostConstruct
+	public void saludo1(){
+		System.out.println("Hola desde la clase inicial");
 	}
 
 }
