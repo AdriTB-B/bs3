53515c4700c1b1e17164c4668bff4cc42ede96c2
Adrián Tena Burgos <adrian.tena@bosonit.com>
2022-05-23 10:09:48 +0200

    Saludo desde la clase secundaria

--- src/main/java/com/adri/bs3/EjercicioBs3SpringApplication.java
+++ src/main/java/com/adri/bs3/EjercicioBs3SpringApplication.java
@@ -1,7 +1,9 @@
 package com.adri.bs3;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import javax.annotation.PostConstruct;
 
@@ -14,6 +16,11 @@ public class EjercicioBs3SpringApplication {
 	@PostConstruct
 	public void saludo1(){
 		System.out.println("Hola desde la clase inicial");
+	}
+
+	@Bean
+	CommandLineRunner saludo2(){
+		return new Segunda();
 	}
 
 }
---
+++ src/main/java/com/adri/bs3/Segunda.java
@@ -0,0 +1,11 @@
+package com.adri.bs3;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class Segunda implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hola desde la clase secundaria");
+    }
+}
