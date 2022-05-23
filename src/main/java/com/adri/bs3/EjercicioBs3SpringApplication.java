ad374954be4f60b171715da335d463774466ecbb
Adrián Tena Burgos <adrian.tena@bosonit.com>
2022-05-23 10:41:56 +0200

    Mostrar valores pasados por parámetro

--- src/main/java/com/adri/bs3/EjercicioBs3SpringApplication.java
+++ src/main/java/com/adri/bs3/EjercicioBs3SpringApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class EjercicioBs3SpringApplication {
-
+	public static String[] valores;
 	public static void main(String[] args) {
+		valores = args;
 		SpringApplication.run(EjercicioBs3SpringApplication.class, args);
 	}
 	@PostConstruct
@@ -24,10 +26,12 @@ public class EjercicioBs3SpringApplication {
 	}
 	@Bean
 	CommandLineRunner saludo3(){
+		Arrays.asList(valores).forEach(System.out::println);
 		return new Tercera();
 	}
 
 }
 //Los mensajes se muestran en orden de creación
 // y PostConstruct que es la primera se ejecuta
-// antes de iniciar el programa en el servidor
+// antes de iniciar el programa en el servidor.
+// Las otras dos su método run se ejecuta al iniciar el programa en el servidor
